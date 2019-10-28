a3d22cca8d90b8cc5238abf589f63ab0861b8cb6
wqlm <649924325@qq.com>
2019-10-28 11:27:57 +0800

    集成mybatis

--- boot-user/src/main/java/com/wqlm/boot/user/BootUserApplication.java
+++ boot-user/src/main/java/com/wqlm/boot/user/BootUserApplication.java
@@ -1,8 +1,10 @@
 package com.wqlm.boot.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.wqlm.boot.user.dao")
 @SpringBootApplication
 public class BootUserApplication {
 
---
+++ boot-user/src/main/java/com/wqlm/boot/user/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wqlm.boot.user.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
