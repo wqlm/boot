7ec751228d57a62f1878cdfb7c4e2b1986413bf3
wqlm <649924325@qq.com>
2019-11-18 15:47:23 +0800

    集成 spring cache

---
+++ boot-user/src/main/java/com/wqlm/boot/user/confing/CacheConfig.java
@@ -0,0 +1,87 @@
+package com.wqlm.boot.user.confing;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+@Configuration
+// 开启 Spring Cache
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport {
+
+    @Value("${cache.expireTime}")
+    // 缓存超时时间
+    private int cacheExpireTime;
+
+    /**
+     * 配置@Cacheable、@CacheEvict等注解在没有指定Key的情况下，key生成策略
+     * 该配置作用于缓存管理器管理的所有缓存
+     * 最终生成的key 为 cache类注解指定的cacheNames::类名:方法名#参数值1,参数值2...
+     *
+     * @return
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(":");
+                sb.append(method.getName());
+                sb.append("#");
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                    sb.append(",");
+                }
+                return sb.substring(0, sb.length() - 1);
+            }
+        };
+    }
+
+
+    /**
+     * 配置缓存管理器
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        // 配置 json 序列化器 - Jackson2JsonRedisSerializer
+        Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jacksonSerializer.setObjectMapper(objectMapper);
+
+        //关键点，spring cache 的注解使用的序列化都从这来，没有这个配置的话使用的jdk自己的序列化，实际上不影响使用，只是打印出来不适合人眼识别
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                // 将 key 序列化成字符串
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 将 value 序列化成 json
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jacksonSerializer))
+                // 设置缓存过期时间，单位秒
+                .entryTtl(Duration.ofSeconds(cacheExpireTime))
+                // 不缓存空值
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+}
